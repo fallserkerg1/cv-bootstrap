2f11cf5b56cbd33049f96a163282a01b0b3e1b6f
yoshua <yoshuasoto12@outlook.com>
2022-03-29 07:02:04 -0400

    Actualizacion 2022

--- index.php
+++ index.php
@@ -47,11 +47,11 @@
     <div class="container">
       <div class="row">
         <div class="cont">
-          <h1>Yoshua Soto | Desarrollador Web</h1>
+          <h1>Soto Yoshua | Desarrollador Web y Analista de Mercados Bursátiles </h1>
           <br>
-          <p class="h5">Me llamo Yoshua Soto, soy Desarrollador Web y Programador BackEnd creativo e innovador, autodidacta, 
-          entusiasta con las grandes ideas y avances del mundo tecnológico, seguidor de los E-sports y las Criptomonedas, 
-          conoce más sobre mi.</p>
+          <p class="h5">Me llamo Yoshua Soto, soy Desarrollador Web y Programador BackEnd, me especializo como Analísta de Sistemas y habilidad en 
+            desarrollo en los Mercados Bursátiles, lee mi web para saber más sobre mi.
+          </p>
           <hr>
           <a href="#contact" class="btn btn-primary btn-lg" id="btn1">Contactame</a>
           <i class="fab fa-twitter"></i>
@@ -69,13 +69,13 @@
         <div class="col-md-6 p-3" id="profil-main">
           <h1>Sobre mi</h1>
           <p>Soy un joven programador autodidacta, siempre estoy al pie del avance de la tecnología, 
-            soy de pensamiento crítico, lógico y persuasivo, en mis ratos libres leo libros o escucho 
-            los audiolibros con temática de economía, política, ciencia, programación, además de ciencia 
-            ficción cyberpunk, distopia, y uno que otro libro sobre cultura japonesa, china y germánica. 
-            Soy entusiasta de los E-Sports y del mundo de las criptomonedas, tradding y minado, creo 
-            firmemente en el avance de la tecnología como muestra de la evolución de la mente humana, 
-            comparto mucho la filosofía de Elon Musk al decir que; "Si hay algo que no va contra las leyes de la física, 
-            puede lograrse".</p>
+            soy de pensamiento crítico, lógico y persuasivo, estudiante de Finanzas, Mercados bursátiles y Acciones,
+            mis estudios se enfocan en el mercado de Forex, Acciones y Criptoactivos.
+            Mi rama principal de la informática es el Análisis de Sistemas en el cual poseo 2 años de experiencia en la misma,
+            lo que me ha permitido desarrollar la habilidad de aprender rápidamente cualquier cosa, puedo decir con toda seguridad
+            que mis dos área en las que me desarollo son el Análisis de Sistemas y los Mercados Bursátiles,
+            en mis ratos libres leo libros o escucho los audiolibros con temática de economía, ciencia, programación, tradding y minado,
+            la investigación es un arte que desarrollé en la Universidad y con el paso del tiempo me ha permitido mejorar como profesional.</p>
             <div class="row mt-5">
               <div class="col-md-6">
                 <h4>Información de Contacto</h4>
@@ -127,7 +127,7 @@
         <div class="col-md-8">
           <h3>Multieliet Company</h3>
           <p>Programador BackEnd • Marzo 2018 - Octubre 2018.</p>
-          <p>El dueño de la compañia me presentó la oferta para trabajar como programador BackEnd PHP usando los frameworks CodeIgniter y Laravel, fue un gran impulso para mi aprendizaje en el mmundo de la programación, aprendí a manejar herramientas como GitHub, duré poco tiempo ya que necesitaba mejorar en mis conocimientos y habilidades en el área del desarrollo web. En la compañia trabajaba de forma remota en Venezuela, ya que la compañia es Venezolana y dirigida por un familiar.</p>
+          <p>El dueño de la compañia me presentó la oferta para trabajar como programador BackEnd PHP usando los frameworks CodeIgniter y Laravel, fue un gran impulso para mi aprendizaje en el mundo de la programación, aprendí a manejar herramientas como GitHub, Laravel, Codeigniter y Javascript.</p>
         </div>
       </div>
       <hr>
@@ -252,43 +252,38 @@
     </div>
   </section>
   <!--FOOTER-->
-  <section id="footer">
-    <div class="container">
-      <div class="list-icon">
-        <!--TWITTER-->
-          <a href="https://twitter.com/KloYoshi"> 
-            <i class="bi bi-twitter"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
-            </svg>
-          </a>
-        <!--FACEBOOK-->
-          <a href="https://www.facebook.com/profile.php?id=100076023571851">
-            <i class="bi bi-facebook"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-              <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-            </svg>
-          </a>
-        <!--GITHUB-->
-          <a href="https://github.com/fallserkerg1?tab=repositories">
-            <i class="bi bi-github"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-            </svg>
-          </a>
-        <!--LINKEDIN-->
-          <a href="https://www.linkedin.com/in/yoshua-soto-oliveros-025818160/">
-            <i class="bi bi-linkedin"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-            </svg>
-          </a>
-      </div>
-        <div class="container mt-5">
-          <p style="text-align: center; color: gray;">Copyright 2022 © | Yoshua Soto</p>
+  <div class="footer-class">
+    <section id="footer">
+      <div class="container">
+        <div class="list-icon">
+          <!--TWITTER-->
+            <a href="https://twitter.com/SotoYoshida"> 
+              <i class="bi bi-twitter"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+              </svg>
+            </a>
+          <!--GITHUB-->
+            <a href="https://github.com/fallserkerg1?tab=repositories">
+              <i class="bi bi-github"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg>
+            </a>           
+          <!--LINKEDIN-->
+            <a href="https://www.linkedin.com/in/yoshua-soto-oliveros-025818160/">
+              <i class="bi bi-linkedin"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg>
+            </a>
         </div>
-    </div>
-  </section>
+          <div class="container mt-5">
+            <p style="text-align: center; color: gray;">Copyright 2022 © | Soto Yoshua</p>
+          </div>
+      </div>
+    </section>
+  </div>
 </body>
     <!--BOOTSTRAP JS-->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -308,4 +303,5 @@
         });
     });
     </script>
+    <button></button>   
 </html>
