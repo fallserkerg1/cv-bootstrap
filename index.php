a717788b262be82b800da0e7053359bed2c942ae
yoshua <yoshuasoto12@outlook.com>
2022-04-06 10:34:53 -0400

    Update 1.5

--- index.php
+++ index.php
@@ -32,7 +32,10 @@
               <a class="nav-link" href="#about">Sobre Mi</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#education">Educación</a>
+              <a class="nav-link" href="#divisor">Informática</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#divisor_2">Trading</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#contact">Contacto</a>
@@ -60,9 +63,49 @@
             Bitcoin (BTC), Solana (SOL) y Tron (TRX).
           </p>
           <hr style="background: white;">
+          <br>
           <a href="#contact" class="btn btn-primary btn-lg" id="btn1">Contactame</a>
           <i class="fab fa-twitter"></i>
         </div>
+        <div class="container">
+          <div class="list-icon_main">
+            <!--FACEBOOK-->
+            <a href="https://www.facebook.com/profile.php?id=100076023571851">
+              <i class="bi bi-facebook"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+              </svg>
+            </a>
+            <!--INSTAGRAM-->
+            <a href="https://www.instagram.com/kloyoshi54/">
+              <i class="bi bi-instagram"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+              </svg>
+            </a>
+            <!--TWITTER-->
+            <a href="https://twitter.com/SotoYoshida"> 
+              <i class="bi bi-twitter"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+              </svg>
+            </a>
+            <!--GITHUB-->
+              <a href="https://github.com/fallserkerg1?tab=repositories">
+                <i class="bi bi-github"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                </svg>
+              </a>           
+            <!--LINKEDIN-->
+              <a href="https://www.linkedin.com/in/yoshua-soto-oliveros-025818160/">
+                <i class="bi bi-linkedin"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                </svg>
+              </a>
+          </div>
+        </div> 
       </div>
     </div>
   </header>
@@ -75,14 +118,18 @@
         </div>
         <div class="col-md-6 p-3" id="profil-main">
           <h1>Sobre mi</h1>
-          <p>Soy un joven programador autodidacta, siempre estoy al pie del avance de la tecnología, 
-            soy de pensamiento crítico, lógico y persuasivo, estudiante de Finanzas, Mercados bursátiles y Acciones,
-            mis estudios se enfocan en el mercado de Forex, Acciones y Criptoactivos.
-            Mi rama principal de la informática es el Análisis de Sistemas en el cual poseo 2 años de experiencia en la misma,
-            lo que me ha permitido desarrollar la habilidad de aprender rápidamente cualquier cosa, puedo decir con toda seguridad
-            que mis dos área en las que me desarollo son el Análisis de Sistemas y los Mercados Bursátiles,
-            en mis ratos libres leo libros o escucho los audiolibros con temática de economía, ciencia, programación, tradding y minado,
-            la investigación es un arte que desarrollé en la Universidad y con el paso del tiempo me ha permitido mejorar como profesional.</p>
+          <p>Inicié en el mundo de la computación a los 6 años, mi primer ordenador era un Pentium II con Windows 98, pero
+            me llamaba la atención el funcionamiento del mismo, así como los videojuegos, ya que mi amor a la tecnología viene
+            por medio de estos, en mi adolescencia y vida universitaria, exploraba las distintas herramientas que mis profesores
+            me enseñaban, tenia mucha habilidad para analizar datos y gestionar bases de datos, el Análisis y Diseño de Sistemas,
+            las metodologías como MEDSI fueron las que me permitieron crear mi tesis de grado, comencé a laburar como Analista de Sistemas 
+            en la Universidad donde estudié, desde allí adquirí 5 años de experiencia en IT, pero mi conocimiento informático aumenta más
+            cuando aprendo a programar en PHP y obtengo un empleo remoto con una pequeña agencia de desarrollo, fue difícil aprender pero luego
+            con la constancia y practica desarrollé mis habilidades lo que me permitió desarrollar mi primera app de escritorio,
+            un Sistema CMS de gestión automatizado de blog, capaz de gestionar usuarios y contenido. Gracias a los conocimientos obtenidos
+          comencé una nueva carrera como Trader y Analista de los Mercados Bursátiles, lo cual me resulta muy interesante y me ha ayudado
+        en mi desarrollo economico y profesional. Actualmente me dedico al desarrollo de páginas web y landing page para agencias de marketing, así
+      como el Análisis del Mercado de FOREX y al leguaje de programación PYTHON.</p>
             <div class="row mt-5">
               <div class="col-md-6">
                 <h4>Información de Contacto</h4>
@@ -100,7 +147,11 @@
       </div>
     </div>
   </section>
-  <!--EDUCATION-->
+    <!--DIVISOR INFORMÁTICA-->
+  <div class="container-fluid" id="divisor">
+      <h1 class="h1" style="padding: 20px;">Carrera Informática</h1>
+  </div>
+  <!--EDUCATION INF-->
   <section id="education">
     <div class="container">
        <!--UNIVERSITY-->
@@ -206,6 +257,103 @@
       </div>
     </div>
   </section>
+    <!--DIVISOR TRADING-->
+    <div class="container-fluid" id="divisor_2">
+      <h1 class="h1" style="padding: 20px;">Carrera Trading</h1>
+    </div>
+    <!--EDUCATION TRADING-->
+    <section id="education">
+      <div class="container">
+         <!--REVIEW-->
+         <div class="row mt-5">
+          <div class="col-md-4">
+            <h1 class="h2" style="padding: 20px;">Resúmen</h1>
+          </div>
+          <div class="col-md-8">
+            <h1>¿Por qué elegí la Trading y los Mercados Bursátiles?</h1>
+            <p>Esta actividad la elegí como una forma de aprender economía y el funcionamiento de la bolsa y los mercados, el tema me pareció fascinante que decidí
+              entrar en este mundo de las inversiones en bolsa, el hacer análisis del los mercados es una actividad que se debe realizar de forma pracmática y realista,
+              el estudio de los mercados para sacar estrategias para llevar a cabo operaciones en bolsa ha sido para mi una experiencia de estudio fascinante a tal punto
+              que es mi segunda carrera profesional.
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h1 class="h2" style="padding: 20px;"> </h1>
+          </div>
+          <div class="col-md-8">
+            <h1>Mercados donde Opero</h1>
+            <p>Elegír el mercado en que operar suele ser una tarea que lleva tiempo, ya que es vital conocerse uno mismo para escoger un mercado en el cual adaptarnos,
+              el mercado nunca se va a adaptar a ti, tu debes adaptarte a el, nunca luches contra el mercado, y tampoco dejes que el mercado te controle contra ti, tu debes fluir, así
+              al estilo de Star Wars, tu debes fluir con el mercado y conocerlo, los mercados donde opero son el <a href="https://es.wikipedia.org/wiki/Mercado_de_divisas" style="text-decoration: none;">FOREX</a>
+              en las divisas EUR/USD, EUR/GBR y EUR/JPY y los <a href="https://es.wikipedia.org/wiki/%C3%8Dndice_burs%C3%A1til" style="text-decoration: none;">Índices</a> como el S&P500.
+            </p>
+          </div>
+        </div>
+         <!--EDUCATION-->
+        <div class="row mt-5">
+          <div class="col-md-4">
+            <h1 class="h2" style="padding: 20px;">Educación</h1>
+          </div>
+          <div class="col-md-8">
+            <h1>Cursos</h1>
+            <h3>MDC Trading Academy</h3>
+            <p>Curso Introductorio de Trading • Marzo 2022</p>
+            <p>Curso básico de Trading, curso en el cual apredí los fundamentos básicos de trading, análisis del mercado, Baktesting, instrumentos financieros y como utilizar los Brokers.</p>
+          </div>
+        </div>
+        <hr>
+        <!--TRADE-->
+        <div class="row mt-5 mb-5">
+          <div class="col-md-4">
+            <h1 class="h2" style="padding: 20px;">Mercados FOREX</h1>
+          </div>
+          <div class="col-md-8">
+            <p>Mercados de Divisas.</p>
+            <div id="skills" class="mt-5">
+              <div class="html">
+                <p class="h4">EUR/USD</p>  
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <br>
+              <div class="css">
+                <p class="h4">EUR/GBP</p>  
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <br>
+              <div class="js">
+                <p class="h4">EUR/JPY</p>  
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <br>
+            </div>
+          </div>
+        </div>
+        <div class="row mt-5 mb-5">
+          <div class="col-md-4">
+            <h1 class="h2" style="padding: 20px;">Mercados Índices</h1>
+          </div>
+          <div class="col-md-8">
+            <p>Mercados de Índices.</p>
+            <div id="skills" class="mt-5">
+              <div class="html">
+                <p class="h4">S&P500</p>  
+                <div class="progress">
+                    <div class="progress-bar bg-success" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+              </div>
+              <br>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
     <!--CONTACT-->
   <section id="contact">
     <div class="container">
